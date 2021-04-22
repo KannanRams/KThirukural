d7e2adeb5dd2a48e9c82d55f86229fe04b44585d
fineclimax <FineClimax@outlook.com>
2021-04-22 17:23:46 +0200

    configure Heroku build1

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const ngApp = express();
 
-ngApp.use(express.static('./dist/kthirukural'));
+ngApp.use(express.static('./docs'));
 
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/kthirukural/index.html'));
+    response.sendFile(path.join(__dirname, '/docs/index.html'));
 });
 
 ngApp.listen(process.env.PORT || 8080);
